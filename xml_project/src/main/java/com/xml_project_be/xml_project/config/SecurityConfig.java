b2627ef243be4cc10a3f55257d42cfed595ad301
Georgii <panovgeorgij819@gmail.com>
2023-09-26 21:24:25 +0500

    refactoring code and update UserController and SecurityConfig

--- xml_project/src/main/java/com/xml_project_be/xml_project/config/SecurityConfig.java
+++ xml_project/src/main/java/com/xml_project_be/xml_project/config/SecurityConfig.java
@@ -1,15 +1,15 @@
 package com.xml_project_be.xml_project.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 
 @Configuration
 @EnableWebSecurity
@@ -20,21 +20,10 @@ public class SecurityConfig {
     private final AuthenticationProvider authProvider;
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf()
-            .disable()
-            .authorizeHttpRequests()
-            .requestMatchers("/api/v1/auth/**")
-            .permitAll()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authenticationProvider(authProvider)
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+    @SneakyThrows
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http.authenticationProvider(authProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
--- xml_project/src/main/java/com/xml_project_be/xml_project/controllers/UserController.java
+++ xml_project/src/main/java/com/xml_project_be/xml_project/controllers/UserController.java
@@ -1,20 +1,11 @@
 package com.xml_project_be.xml_project.controllers;
 
 import com.xml_project_be.xml_project.dao.UserDao;
-import com.xml_project_be.xml_project.dto.UserFilterDTO;
-import com.xml_project_be.xml_project.models.User;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.BindingResult;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-
-@RestController
+@Controller
+@CrossOrigin
 @RequestMapping("/api/v1/users")
 public class UserController {
     UserDao userDao;
@@ -23,56 +14,10 @@ public class UserController {
         this.userDao = userDao;
     }
 
-    @GetMapping("/test")
-    public ResponseEntity<?> Test(){
-        return ResponseEntity.ok("Successful");
-    }
-
+    // test request for check filter request
     @GetMapping
-    @PreAuthorize("hasAuthority('admin:read')")
-    public List<User> getAllUsers(){
-        return userDao.findAll();
-    }
-
-    @PostMapping("/filter")
-    @PreAuthorize("hasAuthority('admin:read')")
-    public List<User> getAllUsersByFilter(@RequestBody UserFilterDTO userFilterDTO){
-        return userDao.filter(
-                userFilterDTO.getFirstName(),
-                userFilterDTO.getLastName(),
-                userFilterDTO.getSecondName(),
-                userFilterDTO.getLogin(),
-                userFilterDTO.getEmail(),
-                userFilterDTO.getRole(),
-                userFilterDTO.getState(),
-                userFilterDTO.getPhoneNumber()
-        );
-    }
-
-    @GetMapping("/{id}")
-    @PreAuthorize("hasAuthority('admin:read')")
-    public Optional<User> getUserById(@PathVariable("id") Long id){
-        return userDao.findById(id);
-    }
-
-    @PostMapping
-    @PreAuthorize("hasAuthority('admin:create')")
-    public ResponseEntity<?> createNewUser(@RequestBody @Valid User user, BindingResult bindingResult){
-        if(bindingResult.hasErrors()) return new ResponseEntity<>(Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage(), HttpStatus.FORBIDDEN);
-        userDao.save(user);
-        return ResponseEntity.ok("Successful create");
-    }
-
-    @DeleteMapping("/{id}")
-    @PreAuthorize("hasAuthority('admin:delete')")
-    public ResponseEntity<?> deleteById(@PathVariable("id") Long id){
-        userDao.deleteById(id);
-        return ResponseEntity.ok("Successful delete");
-    }
-
-    @GetMapping("/login")
-    @PreAuthorize("hasAuthority('admin:read')")
-    public Optional<User> gtUserByLogin(@RequestParam(name = "login") String login){
-        return userDao.findByLogin(login);
+    @ResponseBody
+    public String Test() {
+        return "ResponseEntity.ok(a)";
     }
 }
