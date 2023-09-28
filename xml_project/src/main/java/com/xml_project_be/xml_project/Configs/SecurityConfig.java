04681f126688c36c371758728d39c30a71398b5c
Georgii <panovgeorgij819@gmail.com>
2023-09-28 20:20:49 +0500

    update authorization

--- xml_project/src/main/java/com/xml_project_be/xml_project/Configs/SecurityConfig.java
+++ xml_project/src/main/java/com/xml_project_be/xml_project/Configs/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http = http.formLogin().disable().csrf().disable();
+        http = http.formLogin().disable();
         return http.build();
     }
 }
