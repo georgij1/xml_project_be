c5b481b533b8188232390fcdd87b0503fd2bd414
Georgii <panovgeorgij819@gmail.com>
2023-11-27 00:03:48 +0500

    update ddl

--- xml_project/src/main/resources/db/migration/V3__init.sql
+++ xml_project/src/main/resources/db/migration/V3__init.sql
@@ -1,8 +1,9 @@
 create table files
 (
-    id_image     serial,
-    image_name   text,
-    time_stamp   text,
-    author       text,
-    name_company text
+    id_file serial,
+    file_name text,
+    time_stamp text,
+    author text,
+    name_company text,
+    type_file text
 );
