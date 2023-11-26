0d4ffe7bb05f664e8e5760465e5622723335e5a1
Georgii <panovgeorgij819@gmail.com>
2023-11-27 00:56:10 +0500

    update delete one

--- xml_project/src/main/java/com/xml_project_be/xml_project/file/delete/Delete.java
+++ xml_project/src/main/java/com/xml_project_be/xml_project/file/delete/Delete.java
@@ -1,16 +1,16 @@
 package com.xml_project_be.xml_project.file.delete;
 
+import lombok.SneakyThrows;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Delete {
-    public static ResponseEntity<?> delete(String NameCompany, JdbcTemplate jdbcTemplate, Integer IdFile) throws IOException {
-        Files.delete(Paths.get("/home/georgii/Загрузки/uploads/" + NameCompany + "/" + jdbcTemplate.queryForList("select image_name from files where id_image=?", IdFile).get(0).get("image_name")));
-        jdbcTemplate.update("delete from files where id_image=?", IdFile);
+    @SneakyThrows
+    public static ResponseEntity<?> delete(String NameCompany, JdbcTemplate jdbcTemplate, Integer IdFile) {
+        Files.delete(Paths.get("/home/georgii/Загрузки/uploads/" + NameCompany + "/" + jdbcTemplate.queryForList("select file_name from files where id_file=?", IdFile).get(0).get("image_name")));
+        jdbcTemplate.update("delete from files where id_file=?", IdFile);
         return ResponseEntity.ok().body("Success delete file");
     }
 }
