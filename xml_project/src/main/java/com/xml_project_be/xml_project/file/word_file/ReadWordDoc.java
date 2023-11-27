362c8e06f2b0dcb8cf1ff54d64ae982bee01dfa7
Georgii <panovgeorgij819@gmail.com>
2023-11-27 15:37:54 +0500

    update reader for docx file

--- xml_project/src/main/java/com/xml_project_be/xml_project/file/word_file/ReadWordDoc.java
+++ xml_project/src/main/java/com/xml_project_be/xml_project/file/word_file/ReadWordDoc.java
@@ -1,17 +1,9 @@
 package com.xml_project_be.xml_project.file.word_file;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-
-import com.xml_project_be.xml_project.file.CreateJson.DomainBeanFile;
 import lombok.SneakyThrows;
-import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.usermodel.*;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.http.ResponseEntity;
-
-import static com.xml_project_be.xml_project.file.word_file.ReadWordDocx.getResponseEntity;
+import static com.xml_project_be.xml_project.file.word_file.ReadWordDocMethods.getResponseEntity;
 
 public class ReadWordDoc {
     @SneakyThrows
--- xml_project/src/main/java/com/xml_project_be/xml_project/file/word_file/ReadWordDocx.java
+++ xml_project/src/main/java/com/xml_project_be/xml_project/file/word_file/ReadWordDocx.java
@@ -1,37 +1,30 @@
 package com.xml_project_be.xml_project.file.word_file;
 
 import com.xml_project_be.xml_project.file.CreateJson.DomainBeanFile;
-import lombok.SneakyThrows;
-import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.usermodel.Range;
-import org.jetbrains.annotations.NotNull;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class ReadWordDocx {
-    @SneakyThrows
-    public static ResponseEntity<?> readWordDocx(String NameCompany, JdbcTemplate jdbcTemplate, Integer FileName) {
-        return getResponseEntity(NameCompany, jdbcTemplate, FileName);
-    }
-
-    @NotNull
-    static ResponseEntity<?> getResponseEntity(String NameCompany, JdbcTemplate jdbcTemplate, Integer FileName) {
+    public static ResponseEntity<?> readWordDocx(
+        String NameCompany,
+        JdbcTemplate jdbcTemplate,
+        Integer IDFile
+    ) {
         ArrayList<String> arrayListContentFile = new ArrayList<>();
         DomainBeanFile domainBeanNameFile = new DomainBeanFile();
         domainBeanNameFile.setNameFile(jdbcTemplate.queryForList("select file_name from files").get(0).get("file_name").toString());
         arrayListContentFile.add(String.valueOf((domainBeanNameFile.getNameFile())));
 
-        try {
-            String filePath = "/home/georgii/Загрузки/uploads/" + NameCompany + "/" + jdbcTemplate.queryForList("select file_name from files where id_file=?", FileName).get(0).get("file_name");
-            FileInputStream fis = new FileInputStream(filePath);
-            HWPFDocument document = new HWPFDocument(fis);
-            Range range = document.getRange();
-            arrayListContentFile.add(range.text());
-            fis.close();
+        String filePath = "/home/georgii/Загрузки/uploads/" + NameCompany + "/" + jdbcTemplate.queryForList("select file_name from files where id_file=?", IDFile).get(0).get("file_name");
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            XWPFDocument document = new XWPFDocument(fis);
+            XWPFWordExtractor extractor = new XWPFWordExtractor(document);
+            arrayListContentFile.add(extractor.getText());
         } catch (IOException e) {
             e.printStackTrace();
         }
