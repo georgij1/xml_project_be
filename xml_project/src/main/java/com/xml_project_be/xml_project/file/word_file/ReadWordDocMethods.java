974cd74ccb70316ac2d7c637bb749b7df43b230c
Georgii <panovgeorgij819@gmail.com>
2023-12-10 19:20:34 +0500

    What's new:  1) update word parser (fix bug)

--- xml_project/src/main/java/com/xml_project_be/xml_project/file/word_file/ReadWordDocMethods.java
+++ xml_project/src/main/java/com/xml_project_be/xml_project/file/word_file/ReadWordDocMethods.java
@@ -3,6 +3,8 @@ package com.xml_project_be.xml_project.file.word_file;
 import com.xml_project_be.xml_project.file.CreateJson.DomainBeanFile;
 import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.hwpf.usermodel.Range;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -22,11 +24,13 @@ public class ReadWordDocMethods {
 
         try {
             String filePath = "/home/georgii/Загрузки/uploads/" + NameCompany + "/" + jdbcTemplate.queryForList("select file_name from files where id_file=?", FileName).get(0).get("file_name");
-            File file = new File(filePath);
-            FileInputStream fis = new FileInputStream(file.getPath());
-            HWPFDocument document = new HWPFDocument(fis);
-            Range range = document.getRange();
-            arrayListContentFile.add(range.text());
+            FileInputStream fis = new FileInputStream(filePath);
+            XWPFDocument document = new XWPFDocument(fis);
+
+            // Чтение содержимого по строчно
+            for (XWPFParagraph paragraph : document.getParagraphs()) {
+                arrayListContentFile.add(paragraph.getText());
+            }
             fis.close();
         } catch (IOException e) {
             e.printStackTrace();
